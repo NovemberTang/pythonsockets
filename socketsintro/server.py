bcc516d1cef49766f353d6d1f66987e93b858454
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-01-27 19:36:57 +0000

    time out gracefully

--- socketsintro/server.py
+++ socketsintro/server.py
@@ -2,12 +2,16 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(('localhost', 4571)) #or localhost
-    s.listen(5) # will accept 5 connections before rejecting others
+    
+    s.settimeout(10)
 
-    print('Server is listening for connections')
+    try:
+        s.listen(5) # will accept 5 connections before rejecting others
+        print('Server is listening for connections')
 
-    client, address = s.accept() #blocks the process until a connection is made
-    print(f'Connection from {address} has been established\n')
-    print(f'Client object: {client}\n')
-    client.send(bytes('Welcome to my server', 'utf-8'))
-
+        client, address = s.accept() #blocks the process until a connection is made
+        print(f'Connection from {address} has been established\n')
+        print(f'Client object: {client}\n')
+        client.send(bytes('Welcome to my server', 'utf-8'))
+    except socket.timeout:
+        print('Socket timeout has been exceeded, closing connection')
