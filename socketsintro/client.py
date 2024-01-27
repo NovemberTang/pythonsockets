046231080018213d8a8785a54131167d62744244
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-01-27 19:25:23 +0000

    use with block instead of explicitly closing the connection

--- socketsintro/client.py
+++ socketsintro/client.py
@@ -1,9 +1,7 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 12345))
-s.connect(('localhost', 4571))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('', 12345))
+    s.connect(('localhost', 4571))
 
-message = s.recv(1024) #1024 is the buffer size. It's the max amount of data that can be received at once
-print(message.decode('utf-8')) #decode the message from bytes to string
-
-s.close()
+    message = s.recv(1024) #1024 is the buffer size. It's the max amount of data that can be received at once
+    print(message.decode('utf-8')) #decode the message from bytes to string
--- socketsintro/server.py
+++ socketsintro/server.py
@@ -1,14 +1,13 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 4571)) #or localhost
-s.listen(5) # will accept 5 connections before rejecting others
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('localhost', 4571)) #or localhost
+    s.listen(5) # will accept 5 connections before rejecting others
 
-print('Server is listening for connections')
+    print('Server is listening for connections')
 
-client, address = s.accept() #blocks the process until a connection is made
-print(f'Connection from {address} has been established\n')
-print(f'Client object: {client}\n')
-client.send(bytes('Welcome to my server', 'utf-8'))
+    client, address = s.accept() #blocks the process until a connection is made
+    print(f'Connection from {address} has been established\n')
+    print(f'Client object: {client}\n')
+    client.send(bytes('Welcome to my server', 'utf-8'))
 
-s.close()
