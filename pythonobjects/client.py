54b7dc3129c8a4a43dbfd022a4c3da27ec129c45
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-01-28 08:05:34 +0000

    transfer and deserialise python object

--- pythonobjects/client.py
+++ pythonobjects/client.py
@@ -1,5 +1,6 @@
 import socket
 from product import Product
+import pickle
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(('localhost', 4571))
@@ -9,5 +10,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         if not data:
             print('No data received from server')
             break
-        print('Message from server',data.decode('utf-8'))
         print('Type of data received from server',type(data))
+        unpickled_data = pickle.loads(data)
+        print('Data received from server',unpickled_data)
+        print('Type of deserialized data',type(unpickled_data))
+        print('-----------------------------------')
--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -1,5 +1,6 @@
 import socket
 from product import Product
+import pickle
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(('localhost', 4571))
@@ -15,5 +16,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(f'Connected to {addr}')
     print(f'Client: {client}')
     
-    client.send(bytes(str(python_dictionary), 'utf-8'))
-    client.send(bytes(str(custom_object), 'utf-8'))
+    pickle_dict = pickle.dumps(python_dictionary)
+    pickle_obj = pickle.dumps(custom_object)
+
+    print('Type of pickle_dict', type(pickle_dict))
+    print('Type of pickle_obj', type(pickle_obj))
+
+    client.send(pickle_dict)
+    client.send(pickle_obj)
